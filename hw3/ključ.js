46008fee0b1f3ba0cac37b9b58e3deefb10c6964
danicastefanovic <64682212+danicastefanovic@users.noreply.github.com>
2020-12-25 01:11:15 +0100

    tajmer,brojač pitanja

--- hw3/ključ.js
+++ hw3/ključ.js
@@ -9,3 +9,36 @@ function submitForm(event) {
 function myFunction() {
     location.href = "kviz1.html";
 }
+let question_cnt = 0;
+
+function next() {
+    if (question_cnt <= 9) {
+        question_cnt++;
+    }
+    console.log(question_cnt);
+}
+var timer = 20;
+
+setInterval(function() {
+    timer--;
+
+    if (timer >= 0) {
+        id = document.getElementById("time_left");
+        id.innerHTML = timer;
+    }
+
+    if (timer === 0) {
+        console.log("sledeće Pitanje");
+    }
+}, 1000);
+
+let user_name = localStorage.getItem("firstname");
+let user_points = localStorage.getItem("points");
+document.querySelector("span.firstname").innerHTML = user_name;
+document.querySelector("span.score").innerHTML = user_score;
+
+
+function result() {
+    location.href = "kraj.html";
+
+}
