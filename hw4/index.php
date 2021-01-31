60931c112253805c2526663f2232f87f2cf87fc8
danicastefanovic <64682212+danicastefanovic@users.noreply.github.com>
2021-01-31 22:05:27 +0100

    Registracija
    Povezivanje registracije i baze

--- hw4/index.php
+++ hw4/index.php
@@ -1,3 +1,4 @@
+<?php include('php/signin.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,23 +9,23 @@
 </head>
 <body>
     <div class="container">
-<form method="POST" action="php/signin.php">
+<form  action="php/signin.php" method="POST">
   
 <h2>Registration</h2>
     <div class="input-group">
-    <input type="text" placeholder="Enter your name" name="name">
+    <input type="text" placeholder="Enter your name" name="name" required>
 </div>
 <div class="input-group">
-    <input type="text" placeholder="Enter your surname" name="surname">
+    <input type="text" placeholder="Enter your surname" name="surname" required>
 </div>
 <div class="input-group">
-    <input type="text" placeholder="Enter your username" name="username">
+    <input type="text" placeholder="Enter your username" name="username" required>
 </div>
 <div class="input-group">
-    <input type="text" placeholder="Enter your email" name="usermail">
+    <input type="text" placeholder="Enter your email" name="email" required>
 </div>
     <div class="input-group">
-    <input type="password" placeholder="Enter your password" name="password">
+    <input type="password" placeholder="Enter your password" name="password" required>
 </div>
     <div class="input-group">
      <button type="submit" class="btn" name="register">Sign up</button>
