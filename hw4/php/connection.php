58e097927eab80ff712762ab9b316be590d6af9f
danicastefanovic <64682212+danicastefanovic@users.noreply.github.com>
2021-01-31 14:36:19 +0100

    Update konekcije
    S obzirom da prva konekcija sa bazom podataka nije uspela,pokušavam na ovaj način.

--- hw4/php/connection.php
+++ hw4/php/connection.php
@@ -1,58 +1,17 @@
 <?php
+<?php
 session_start();
 
-$name = filter_input(INPUT_POST, 'name');
-$surname = filter_input(INPUT_POST, 'surname');
-$username = filter_input(INPUT_POST, 'username');
-$usermail = filter_input(INPUT_POST, 'usermail');
-$password = filter_input(INPUT_POST, 'password');
-if (!empty($name)){
-if (!empty($surname)){
-if (!empty($username)){
-if (!empty($usermail)){
-if (!empty($password)){
-
-
-$host = "localhost";
-$dbname="";
-$dbsurname="";
-$dbusername = "root";
-$dbusermail="";
-$dbpassword="";
+$servername = "localhost";
+$username = "root";
+$password = "";
 $dbname = "registration";
 
-$conn = new mysqli ($host,$dbname,$dbsurname, $dbusername,$dbusermail, $dbpassword, $dbname);
+$mysqli = new mysqli($servername, $username, $password, $dbname);
 
-if (mysqli_connect_error()){
-    die('Connect Error ('. mysqli_connect_errno() .') '
-    . mysqli_connect_error());
-    }
-    else{
-    $sql = "INSERT INTO users (name,surname,username,usermail, password)
-    values ('$name','$surname','$username','$usermail','$password')";
-    if ($conn->query($sql)){
-        header('location:../admin/index.php');
-    }
-    else{
-        echo "Error: ". $sql ."
-        ". $conn->error;
-        }
-        $conn->close();
-        }
-    }
+if($mysqli->connect_error) {
+    die("Connection is done: " . $mysqli->connect_error);
 }
-        }
-        else{
-        echo "Password should not be empty";
-        header('location:../phpprojekat/login.php');
-        die();
-        }
-        }
-    }
-        else{
-        echo "Username should not be empty";
-        header('location:../phpprojekat/login.php');
-        die();
-        }
+
 
 ?>
